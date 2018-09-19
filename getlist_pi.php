6a594c540a3215140527d2aad1d8453028fb287e
Ganang Sadewo <ganang59@gmail.com>
2018-09-19 14:58:49 +0700

    Update getlist_PI

--- getlist_pi.php
+++ getlist_pi.php
@@ -2,37 +2,22 @@
 
 include_once "koneksi.php";
 
-$response = array();
-// // include db connect class
-// mysqli_connect("localhost","root","");
-// mysqli_select_db("dbregulations");
+$sql = "select * from tbl_pi";
  
-//  get by pi
-
-class usr {}
-$result = mysqli_query($con, "SELECT judul FROM tbl_pi") or die(mysqli_error());
+$res = mysqli_query($con,$sql);
  
-// cek
-if (mysqli_num_rows($result) > 0) {
-    // looping hasil
-    // pi node
-    $response["data"] = array();
-     
-    while ($row = mysqli_fetch_array($result)) {
-        $pi = array();
-        $pi["judul"] = $row[0];
-        // masukan pi pada $response
-        array_push($response["data"], $pi);
-    }
-    // sukses
-    $response["success"] = 1;
+$result = array();
  
-    // echo JSON response
-    echo json_encode($response);
-} else {
-    $response["success"] = 0;
-    $response["message"] = "Tidak ada data yang ditemukan";
- 
-    echo json_encode($response);
+while($row = mysqli_fetch_array($res)){
+array_push($result,
+array('judul'=>$row[0],
+'link'=>$row[1],
+'konten'=>$row[2]
+));
 }
+ 
+echo json_encode(array("result"=>$result));
+ 
+mysqli_close($con);
+ 
 ?>
