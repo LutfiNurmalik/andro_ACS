9f25a2f6ab1d27cc5ae76e5d958d75e172430730
Ganang Sadewo <ganang59@gmail.com>
2018-09-20 21:06:11 +0700

    Update getlist_psn.php

--- getlist_psn.php
+++ getlist_psn.php
@@ -2,47 +2,45 @@
 
 include_once "koneksi.php";
 
-$response = array();
-// // include db connect class
-// mysqli_connect("localhost","root","");
-// mysqli_select_db("dbregulations");
+
+
+$psn = $_GET['psn'];
+
+// $un_id = array_key_exists('un_id', $_POST) ? $_POST['un_id'] : "";
+// $psn = array_key_exists('psn', $_POST) ? $_POST['psn'] : "";
+
+$sql = "select * from tbl_psn where psn = '$psn' ";
  
-//  get by classification
-//hai
-class usr {}
-$result = mysqli_query($con, "SELECT * FROM tbl_psn WHERE 1") or die(mysqli_error());
+$result = mysqli_query($con, $sql);
  
-// cek
-if (mysqli_num_rows($result) > 0) {
-    // looping hasil
-    // classification node
-    $response["data"] = array();
-     
-    while ($row = mysqli_fetch_array($result)) {
-        $psn = array();
-        $psn["un_id"] = $row[0];
-        $psn["psn"] = $row[1];
-        $psn["class"] = $row[2];
-        $psn["hazard"] = $row[3];
-        $psn["pg"] = $row[4];
-        $psn["pa_pi"] = $row[5];
-        $psn["pa_net_qty"] = $row[6];
-        $psn["cao_pi"] = $row[7];
-        $psn["cao_net_qty"] = $row[8];
-        $psn["sp"] = $row[9];
-        $psn["erg"] = $row[10];
-        // masukan classification pada $response
-        array_push($response["data"], $psn);
-    }
-    // sukses
-    $response["success"] = 1;
+while(($row = mysqli_fetch_assoc($result)) == true){
  
-    // echo JSON response
-    echo json_encode($response);
-} else {
-    $response["success"] = 0;
-    $response["message"] = "Tidak ada data yang ditemukan";
- 
-    echo json_encode($response);
+ $data[]=$row;
 }
+echo json_encode($data);
+
+// $res = mysqli_query($con,$sql);
+ 
+// $result = array();
+ 
+// while($row = mysqli_fetch_array($res)){
+// array_push($result,
+// array('un_id'=>$row[0],
+// 'psn'=>$row[1],
+// 'class'=>$row[2],
+// 'hazard'=>$row[3],
+// 'pg'=>$row[4],
+// 'pa_pi'=>$row[5],
+// 'pa_net_qty'=>$row[6],
+// 'cao_pi'=>$row[7],
+// 'cao_net_qty'=>$row[8],
+// 'sp'=>$row[9],
+// 'erg'=>$row[10]
+// ));
+// }
+ 
+// echo json_encode(array("result"=>$result));
+ 
+// mysqli_close($con);
+ 
 ?>
